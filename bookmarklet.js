387d9e6071d9723981ff33dedde677d606d51b46
Ed Cottrell <emc@edcottrell.com>
2014-04-29 15:23:49 -0500

    Update bookmarklet.js
    Wrap code in a function to avoid name conflicts

--- bookmarklet.js
+++ bookmarklet.js
@@ -24,35 +24,39 @@ javascript:
  * THE SOFTWARE.
  */
  
-var domain = "http://www.search.txcourts.gov/", /* Domain for the searches */
-	path = "CaseSearch.aspx?coa=cossup", /* Initialize the URL to use if we cannot do a search */
-	gs = window.getSelection(); /* Get the selection */
- 
-/* See if the user has actually selected anything */
-if(gs.type != "None") {
-	var caseNoContainer = gs.getRangeAt(0), /* Get the selected range */
-		sc = caseNoContainer.startContainer; /* Get the data container for the selection */
-	if(typeof(sc.data) != "undefined") {
-		/* Get the raw selection */
-		var caseNo = sc.data.substr(caseNoContainer.startOffset, caseNoContainer.endOffset - caseNoContainer.startOffset);
-	
-		/* Replace en-dashes with hyphens and strip any leading or trailing characters */
-		caseNo = caseNo.replace(/\u2013/g, "-").replace(/^[^\d]+|[^CRV\d]+$/g, "");
-	
-		/* If the case is a -CR or -CV case, make sure the numeric parts are appropriately padded with leading zeroes */
-		if(caseNo.match(/C[RV]$/)) {
-			caseNoParts = caseNo.split("-");
-			caseNoParts[0] = ("00"+caseNoParts[0]).slice(-2);
-			caseNoParts[1] = ("00"+caseNoParts[1]).slice(-2);
-			caseNoParts[2] = ("00000"+caseNoParts[2]).slice(-5);
-			caseNo = caseNoParts.join("-");
-		}
+function TAMESSearch() {
+	var domain = "http://www.search.txcourts.gov/", /* Domain for the searches */
+		path = "CaseSearch.aspx?coa=cossup", /* Initialize the URL to use if we cannot do a search */
+		gs = window.getSelection(); /* Get the selection */
+	 
+	/* See if the user has actually selected anything */
+	if(gs.type != "None") {
+		var caseNoContainer = gs.getRangeAt(0), /* Get the selected range */
+			sc = caseNoContainer.startContainer; /* Get the data container for the selection */
+		if(typeof(sc.data) != "undefined") {
+			/* Get the raw selection */
+			var caseNo = sc.data.substr(caseNoContainer.startOffset, caseNoContainer.endOffset - caseNoContainer.startOffset);
 		
-		/* If the case number is non-empty, get the URL for the search page. */
-		if(caseNo != "") {
-			var path = "Case.aspx?cn="+caseNo;
+			/* Replace en-dashes with hyphens and strip any leading or trailing characters */
+			caseNo = caseNo.replace(/\u2013/g, "-").replace(/^[^\d]+|[^CRV\d]+$/g, "");
+		
+			/* If the case is a -CR or -CV case, make sure the numeric parts are appropriately padded with leading zeroes */
+			if(caseNo.match(/C[RV]$/)) {
+				caseNoParts = caseNo.split("-");
+				caseNoParts[0] = ("00"+caseNoParts[0]).slice(-2);
+				caseNoParts[1] = ("00"+caseNoParts[1]).slice(-2);
+				caseNoParts[2] = ("00000"+caseNoParts[2]).slice(-5);
+				caseNo = caseNoParts.join("-");
+			}
+			
+			/* If the case number is non-empty, get the URL for the search page. */
+			if(caseNo != "") {
+				var path = "Case.aspx?cn="+caseNo;
+			}
 		}
 	}
+	/* Open the search window */
+	window.open(domain+path);
 }
-/* Open the search window */
-window.open(domain+path);
+
+TAMESSearch();
